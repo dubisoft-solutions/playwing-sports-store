a68c4e2d9b65b3dd8f9882cee9adf697dfb911ff
Ivan Dubynets <ivan.dubynets@gmail.com>
2024-01-10 18:21:33 +0100

    add scroll top

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,7 @@ $(function() {
     addScrolledClassToNavbarOnScroll();
     carouselsSetup(document.documentElement.getAttribute("dir") == 'rtl');
     initRecaptcha('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', ".grecaptcha");
+    initScrollToTopBtnHandler();
 });
 
 function initSelectPicker() {
@@ -282,7 +283,9 @@ $(function() {
     });
 });
 
-
+/**
+ * Recaptcha
+ */
 function initRecaptcha(sitekey, containerSelector) {
     const recaptchaContainers = document.querySelectorAll(containerSelector);
     recaptchaContainers.forEach(recaptchaContainer => {
@@ -295,4 +298,36 @@ function initRecaptcha(sitekey, containerSelector) {
 
         });
     })
+}
+
+function initScrollToTopBtnHandler() {
+    var scrollToTopButtons = document.querySelectorAll('.scroll-to-top-btn');
+    if (!scrollToTopButtons.length) return;
+
+    scrollToTopButtons.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            scrollToTop(500);
+        });
+    });
+
+    function scrollToTop(durationMs) {
+        // cancel if already on top
+        if (document.scrollingElement.scrollTop === 0) return;
+
+        const cosParameter = document.scrollingElement.scrollTop / 2;
+        let scrollCount = 0,
+            oldTimestamp = null;
+
+        function step(newTimestamp) {
+            if (oldTimestamp !== null) {
+                // if duration is 0 scrollCount will be Infinity
+                scrollCount += Math.PI * (newTimestamp - oldTimestamp) / durationMs;
+                if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
+                document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
+            }
+            oldTimestamp = newTimestamp;
+            window.requestAnimationFrame(step);
+        }
+        window.requestAnimationFrame(step);
+    }
 }
